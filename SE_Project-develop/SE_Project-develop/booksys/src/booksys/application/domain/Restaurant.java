9a4fcd1758caec6a70accec6223577f2b158ac9c
jangeunha1119 <jjenny1119@kyonggi.ac.kr>
2021-05-16 23:10:09 +0900

    java파일만 업로드했습니다. 노쇼기능인 Customer와 연관된 클래스 위주로 수정.

---
+++ SE_Project-develop/SE_Project-develop/booksys/src/booksys/application/domain/Restaurant.java
@@ -0,0 +1,59 @@
+/*
+ * Restaurant Booking System: example code to accompany
+ *
+ * "Practical Object-oriented Design with UML"
+ * Mark Priestley
+ * McGraw-Hill (2004)
+ */
+
+package booksys.application.domain;
+
+import booksys.application.persistency.*;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Vector;
+
+class Restaurant {
+	BookingMapper bm = BookingMapper.getInstance();
+	CustomerMapper cm = CustomerMapper.getInstance();
+	TableMapper tm = TableMapper.getInstance();
+
+	Vector getBookings(Date date) {
+		return bm.getBookings(date);
+	}
+
+	// noshow, blacklist 추가
+	Customer getCustomer(String name, String phone, int noshow, boolean blacklist) {
+		return cm.getCustomer(name, phone, noshow, blacklist);
+	}
+
+	Table getTable(int n) {
+		return tm.getTable(n);
+	}
+
+	static Vector getTableNumbers() {
+		return TableMapper.getInstance().getTableNumbers();
+	}
+
+	// noshow, blacklist 추가
+	public Booking makeReservation(int covers, Date date, Time time, int tno, String name, String phone, int noshow,
+			boolean blacklist) {
+		Table t = getTable(tno);
+		Customer c = getCustomer(name, phone, noshow, blacklist);
+		return bm.createReservation(covers, date, time, t, c, null);
+	}
+
+	public Booking makeWalkIn(int covers, Date date, Time time, int tno) {
+		Table t = getTable(tno);
+		return bm.createWalkIn(covers, date, time, t);
+	}
+
+	public void updateBooking(Booking b) {
+		bm.updateBooking(b);
+	}
+
+	public void removeBooking(Booking b) {
+		bm.deleteBooking(b);
+	}
+}
